4a488a4b2b55f8629e4f29b7eb0fc6d145da62e8
ri-commits <kartashourychard@gmail.com>
2023-05-25 18:30:26 +0300

    Page-Form Done

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,9 +5,11 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
-import Router from "next/router";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
@@ -29,7 +31,7 @@ const CreatePrompt = () => {
       });
 
       if (response.ok) {
-        Router.push("/");
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
