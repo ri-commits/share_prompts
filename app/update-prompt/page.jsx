a6697f8ea91abe77a8761d2909a30c5f6d8667a3
ri-commits <kartashourychard@gmail.com>
2023-05-27 10:16:51 +0300

    Edit prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -30,36 +30,38 @@ const EditPrompt = () => {
     if (promptId) getPromptDetails();
   }, [promptId]);
 
-  //   const createPrompt = async (e) => {
-  //     e.preventDefault();
-  //     setSubmitting(true);
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-  //     try {
-  //       const response = await fetch("/api/prompt/new", {
-  //         method: "POST",
-  //         body: JSON.stringify({
-  //           prompt: post.prompt,
-  //           userId: session?.user.id,
-  //           tag: post.tag,
-  //         }),
-  //       });
+    if (!promptId) return alert("Prompt ID not found");
 
-  //       if (response.ok) {
-  //         router.push("/");
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setSubmitting(false);
-  //     }
-  //   };
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <Form
       type="Edit"
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={() => {}}
+      handleSubmit={updatePrompt}
     />
   );
 };
