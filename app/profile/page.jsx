9fb6e49e955348cc76eb7060e65711daa8e007ee
ri-commits <kartashourychard@gmail.com>
2023-05-27 09:16:36 +0300

    Profile functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,30 +7,31 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
-  const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
 
-      setPosts(data);
+      setMyPosts(data);
     };
 
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session?.user.id]);
 
   const handleEdit = () => {};
 
   const handleDelete = async () => {};
 
   return (
-    <MyProfile
+    <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={posts}
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
